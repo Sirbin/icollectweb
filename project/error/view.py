9e4d22a1e6d8a0ed85ec5c2f6d21211ae7f93dce
alessio Bino <alessio.bino@gmail.com>
2015-09-07 16:19:40 +0200

    Signed-off-by: alessio Bino <alessio.bino@gmail.com>

--- project/error/view.py
+++ project/error/view.py
@@ -12,6 +12,6 @@ def page_not_found(e):
     return render_template('404.html', current_time=datetime.utcnow()), 404
 
 
-@blueprints_error.errorhandler(500)
-def server_internal_error(e):
-    return render_template('500.html', current_time=datetime.utcnow()), 500
+# @blueprints_error.errorhandler(500)
+# def server_internal_error(e):
+#     return render_template('500.html', current_time=datetime.utcnow()), 500
