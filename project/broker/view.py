dd99d9658adfbb30083a182e1a8f1fd2e4c53273
alessio Bino <alessio.bino@gmail.com>
2015-10-17 21:37:34 +0200

    change page meters

--- project/broker/view.py
+++ project/broker/view.py
@@ -2,13 +2,14 @@ __author__ = 'Alessio'
 
 from datetime import datetime
 from functools import wraps
-from project import app
+from project import db
+from  project.model_ import gauge_
 from flask import render_template, redirect, url_for, session, flash, Blueprint,copy_current_request_context
 from project.json_conf_ import create_tenant_building,create_json_table,create_json_building,create_json_tne
-from Mqtt_and_socket import controllo_broker,create_json_publish
+from Mqtt_and_socket import controllo_broker
 import threading
 from flask_login import login_required,current_user
-from flask_socketio import SocketIO,join_room,emit
+
 
 #configuration Json
 json_tne_number = create_json_tne()
@@ -25,29 +26,19 @@ blueprint_mqtt_data = Blueprint('Mqtt',__name__)
 @login_required
 def tables_history_meters(meters):
     if json_building != False:
-        for tne in json_table_meter_value:
-            if meters == tne['tne_number']:
-                session['tne'] = meters
-                print session['tne']
-                #print "la base", request.url[43 :]
-                myth = threading.Thread(target=controllo_broker,args=(meters,))
-                #print "nome" ,myth.getName()
-                myth.start()
-                # prova1 = dict({meters:myth})
-                # my_data.prova.append(prova1)
-                # print "lista",my_data.prova
-                # print "lista2",my_data.prova[0]
-                #
-                # for c in my_data.prova:
-                #     for key, value in c.items():
-                #
-                #             #value.join()
-
-
-
-                return render_template('meters_name.html',tne=meters,json_building=json_building,
-                                       tenant = json_tenant_building,
-                                       current_time=datetime.utcnow())
+        user_gauge_model_id = current_user.id_user
+        gauge_query_id_user = db.session.query(gauge_).filter_by(id_=user_gauge_model_id)
+        if gauge_query_id_user is not None:
+            for t in gauge_query_id_user:
+                 print t.id_, t.name_gauge_change, t.gauge_choiche
+            for tne in json_table_meter_value:
+                if meters == tne['tne_number']:
+                    session['tne'] = meters
+                    myth = threading.Thread(target=controllo_broker,args=(meters,))
+                    myth.start()
+                    return render_template('meters_name.html',tne=meters,json_building=json_building,
+                                       tenant = json_tenant_building,json_table_meter_value=json_table_meter_value,current_time=datetime.utcnow(),
+                                           value_boolen_gauge_id = t.gauge_choiche,value_name_gauge_id=t.name_gauge_change)
     flash("Json not found")
     return redirect(url_for('dashboard_'))
 
